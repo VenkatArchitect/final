e3bca39ce79f7ee94c6c7d3b061bdcb3bc575c39
Venkat Ramakrishnan <venkat.architect@gmail.com>
2022-09-12 00:42:14 +0530

    Wrote unit test for get_dataset_from_s3.

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -1,5 +1,6 @@
 import mlflow
-from exp import get_experiment_id
+from exp import get_experiment_id, get_dataset_from_s3
+
 
 #
 # test_initialize_flow is a UNIT test. It asserts for
@@ -8,11 +9,19 @@ from exp import get_experiment_id
 # function before mlflow experiments are started.
 #
 def test_initialize_mlflow():
-
     mlflow.set_experiment(get_experiment_id())
 
     experiment = mlflow.get_experiment_by_name(get_experiment_id())
-    assert(experiment.name == get_experiment_id())
-    assert(experiment.tags == {})
-    assert(experiment.lifecycle_stage == 'active')
-    assert(mlflow.MlflowClient() != None)
+    assert (experiment.name == get_experiment_id())
+    assert (experiment.tags == {})
+    assert (experiment.lifecycle_stage == 'active')
+    assert (mlflow.MlflowClient() != None)
+
+BUCKET_NAME = 'tf-dataset-bucket'
+DATA_FILE_NAME = 'iris-data.csv'
+
+#
+# test_get_dataset_from_s3 is a UNIT test. It asserts
+# that the size of the dataset read from s3 is non-zero.
+def test_get_dataset_from_s3():
+    assert (len(get_dataset_from_s3(BUCKET_NAME, DATA_FILE_NAME)) != 0)
