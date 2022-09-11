b868e34212b5374d55daad19e25b824122702cbc
Venkat Ramakrishnan <venkat.architect@gmail.com>
2022-09-12 00:55:20 +0530

    Wrote unit test for prepare_data

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -1,5 +1,5 @@
 import mlflow
-from exp import get_experiment_id, get_dataset_from_s3
+from exp import get_experiment_id, get_dataset_from_s3, prepare_data
 
 
 #
@@ -17,11 +17,24 @@ def test_initialize_mlflow():
     assert (experiment.lifecycle_stage == 'active')
     assert (mlflow.MlflowClient() != None)
 
+
 BUCKET_NAME = 'tf-dataset-bucket'
 DATA_FILE_NAME = 'iris-data.csv'
+
 
 #
 # test_get_dataset_from_s3 is a UNIT test. It asserts
 # that the size of the dataset read from s3 is non-zero.
 def test_get_dataset_from_s3():
     assert (len(get_dataset_from_s3(BUCKET_NAME, DATA_FILE_NAME)) != 0)
+
+
+def test_prepare_data():
+    (x_train, y_train, x_test, y_test, train, test) = prepare_data()
+
+    xtrain_columns_list = x_train.columns.values.tolist()
+    assert ("class" not in xtrain_columns_list)
+
+    ytrain_columns_list = y_train.to_frame().columns.values.tolist()
+    assert ("class" in ytrain_columns_list)
+    assert (len(ytrain_columns_list) == 1)
